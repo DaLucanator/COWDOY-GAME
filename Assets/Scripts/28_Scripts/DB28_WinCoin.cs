bc885569f506d6db433ba7e19e2e7d439ceec6e1
Daniel Black <1024175@student.sae.edu.au>
2021-10-14 14:20:24 +0800

    Movement, Sprite, and UI fix
    Made a different script for movement, added a player sprite, and fixed the scale to suit the UI

--- Assets/Scripts/28_Scripts/DB28_WinCoin.cs
+++ Assets/Scripts/28_Scripts/DB28_WinCoin.cs
@@ -6,19 +6,19 @@ public class DB28_WinCoin : MonoBehaviour
 {
     public Vector3 velocity;
     public float speed;
-    public float rotation;
 
-    public float spinSpeed = 100f;
+    //public float rotation;
+    //public float spinSpeed = 100f;
 
-    void Start()
-    {
-        transform.rotation = Quaternion.Euler(0, 0, rotation);
-    }
+    //void Start()
+    //{
+        //transform.rotation = Quaternion.Euler(0, 0, rotation);
+    //}
 
     void Update()
     {
         transform.Translate(velocity * speed * Time.deltaTime);
-        this.transform.Rotate(Time.deltaTime * this.spinSpeed, 0f, 0f);
+        //this.transform.Rotate(Time.deltaTime * this.spinSpeed, 0f, 0f);
     }
 
     private void OnTriggerEnter(Collider collision)

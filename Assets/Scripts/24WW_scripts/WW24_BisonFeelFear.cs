15372d15a61992f88f7d9dbefefaf80b680510da
Woody <1024800@student.sae.edu.au>
2021-10-25 13:22:51 +0800

    Herding Mechanic done
    All that's left is Assets and Win/Lose conditions

--- Assets/Scripts/24WW_scripts/WW24_BisonFeelFear.cs
+++ Assets/Scripts/24WW_scripts/WW24_BisonFeelFear.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class WW24_BisonFeelFear : MonoBehaviour
 {
+    public GameObject stats;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerStay(Collider other)
     {
-        
+        if (other.tag == "WW24Mouse")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, other.transform.position, -speed);
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
+        if (other.tag == "WW24Pen")
+        {
+            Destroy(this.gameObject);
+            stats.GetComponent<WW24_GameInformation>().upCollected();
+        }
     }
+
 }
+
--- Assets/Scripts/24WW_scripts/WW24mousePointer.cs
+++ Assets/Scripts/24WW_scripts/WW24mousePointer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mousePointer : MonoBehaviour
+public class WW24mousePointer : MonoBehaviour
 {
     public Transform temporary;
     public Vector3 mousePos;

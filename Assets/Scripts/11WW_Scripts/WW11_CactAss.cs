68745ec15730811863e8318bf50817ec82cf0960
Woody <1024800@student.sae.edu.au>
2021-11-10 14:31:47 +0800

    Cactus Attack'd Us!
    basic visual mechanic, three objects, one's different, click that one, DONE!

---
+++ Assets/Scripts/11WW_Scripts/WW11_CactAss.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WW11_CactAss : MonoBehaviour
+{
+    public GameObject badBrain;
+    public int thisCactus;
+    public bool hover;
+    public bool click;
+    public Material m1;
+    public Material m2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.thisCactus == badBrain.GetComponent<WW11_badBrain>().choose)
+        {
+            this.GetComponent<Renderer>().material = m2;
+        }
+        else
+        {
+            this.GetComponent<Renderer>().material = m1;
+
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            click = true;
+        }
+        if (hover && this.thisCactus == badBrain.GetComponent<WW11_badBrain>().choose && Input.GetMouseButtonDown(0))
+        {
+            Win();
+        }
+
+        
+        //if timer runout, Lose();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hover = true;
+        }
+    }
+
+    public void Win()
+    {
+        Debug.Log("Win");
+    }
+
+    public void Lose()
+    {
+        Debug.Log("Lose");
+    }
+}
---
+++ Assets/Scripts/11WW_Scripts/WW11_badBrain.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WW11_badBrain : MonoBehaviour
+{
+    public int choose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        choose = Random.Range(1, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

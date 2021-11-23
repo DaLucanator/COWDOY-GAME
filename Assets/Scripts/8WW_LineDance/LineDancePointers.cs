4476eae7e7836859b6f42d65fb8517b3850e0c27
Woody <1024800@student.sae.edu.au>
2021-11-23 14:57:29 +0800

    Luc Push from class

---
+++ Assets/Scripts/8WW_LineDance/LineDancePointers.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDancePointers : MonoBehaviour
+{
+    public GameObject bustaMove;
+    public Material poseLeft;
+    public Material poseUp;
+    public Material poseRight;
+    public Material poseDown;
+    public int positionChoice;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionChoice = bustaMove.GetComponent<LineDancePoseScript>().positionChoice;
+        if (positionChoice == 1)
+        {
+            this.GetComponent<Renderer>().material = poseLeft;
+        }
+        else if (positionChoice == 2)
+        {
+            this.GetComponent<Renderer>().material = poseUp;
+
+        }
+        else if (positionChoice == 3)
+        {
+            this.GetComponent<Renderer>().material = poseRight;
+
+        }
+        else
+        {
+            this.GetComponent<Renderer>().material = poseDown;
+
+        }
+    }
+}

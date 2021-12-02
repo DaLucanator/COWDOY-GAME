1c41a7333478d70046c600875bb733b8f526640d
Luc <lucgrfths@gmail.com>
2021-12-02 12:52:33 +0800

    Recovered old scripts 7_Asteroids

---
+++ Assets/Scripts/7_Scripts/Old Script Versions/DB7_PlayerOLD.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DB7_PlayerOLD : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private bool thrust;
+    private bool reverse;
+    private float turnDirection;
+
+    public DB7_Bullet bulletPrefab;
+    public float moveSpeed;
+    public float turnSpeed;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        thrust = (Input.GetKey(KeyCode.W));
+        reverse = (Input.GetKey(KeyCode.S));
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            turnDirection = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+            turnDirection = -1f;
+        else
+        {
+            turnDirection = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust)
+        {
+            rigidbody.AddForce(this.transform.up * this.moveSpeed);
+        }
+        if (reverse)
+        {
+            rigidbody.AddForce(this.transform.up * this.moveSpeed * -1f);
+        }
+        if (turnDirection != 0f)
+        {
+            rigidbody.AddTorque(turnDirection * turnSpeed);
+        }
+    }
+
+    private void Shoot()
+    {
+        DB7_Bullet bullet = Instantiate(this.bulletPrefab, this.transform.position, this.transform.rotation);
+        bullet.Project(this.transform.up);
+    }
+
+
+    bool gamelose = true;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Hostile")
+        {
+            if (gamelose == true)
+            {
+                //Player Loss State
+                print("You Lose...");
+                gamelose = false;
+                rigidbody.velocity = Vector3.zero;
+                rigidbody.angularVelocity = 0f;
+                this.gameObject.SetActive(false);
+                GameController.current.ReturnToMain(false);
+            }
+        }
+    }
+}
